0eb42afc63806857002056d14b96979364c68444
Junior Abranches <jr-abranches@hotmail.com>
2018-01-15 22:52:56 -0300

    Update NewsPage and RemindersPage

--- src/pages/news/news.ts
+++ src/pages/news/news.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import {LoadingProvider} from '../../providers/loading/loading'
+
 @Component({
   selector: 'page-news',
   templateUrl: 'news.html'
 })
 export class NewsPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public loading: LoadingProvider) {
 
+  }
+
+  change(segmento){
+    this.loading.presentComMensagem('Carregando Mensagem');
+    if(segmento === 'ing') {
+
+    } else {
+
+    }
+    this.loading.dismiss();
   }
 
 }
